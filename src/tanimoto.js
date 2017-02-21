409178e860af9781ac53020327e8ec4577be6f39
Daniel Kostro <kostro.d@gmail.com>
2017-02-21 10:05:21 +0100

    change api method: keywordSimilarity -> valueSimilarity

--- src/tanimoto.js
+++ src/tanimoto.js
@@ -8,6 +8,6 @@ var binary = require('./binary');
 exports.binarySimilarity = binary.similarity;
 exports.binaryDistance = binary.distance;
 
-var keyword = require('./keyword');
-exports.keywordSimilarity = keyword.similarity;
-exports.keywordDistance = keyword.distance;
+var value = require('./value');
+exports.valueSimilarity = value.similarity;
+exports.valueDistance = value.distance;
